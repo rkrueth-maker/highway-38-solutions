b98debec20f4662ba15e2d85a5f4b95b93dbeb37
rkrueth-maker <297361570+rkrueth-maker@users.noreply.github.com>
2026-08-27 21:40:43 -0500

    Add v2.6.5 Facebook guest extraction regression fixtures

--- supabase/functions/reseller-facebook-public-v240/core.test.mjs
+++ supabase/functions/reseller-facebook-public-v240/core.test.mjs
@@ -19,4 +19,17 @@ const onlyVerified=normalizeMarketplaceRows([{id:'3',title:'Unproven',location_l
 const html='<script>{"marketplace_listing_title":"Tool Chest","formatted_amount":"$100","location":"Duluth, MN","uri":"https://img.example/x.jpg"}</script><a href="/marketplace/item/987654321/">x</a>';
 const parsed=parseGuestHtml(html,{radiusMiles:50,locationLabel:'Duluth, Minnesota'});assert.equal(parsed.length,1);assert.equal(parsed[0].url,'https://www.facebook.com/marketplace/item/987654321/');assert.equal(parsed[0].location_verified,true);
 
-console.log('PASS reseller-facebook-public-v260 generic public-location fixtures');
+const slashEscaped='<script>{"marketplace_listing_title":"M18 Drill","formatted_amount":"85","location":"Grand Rapids, MN","canonical_url":"https:\\/\\/www.facebook.com\\/marketplace\\/item\\/111222333444\\/"}</script>';
+const escapedParsed=parseGuestHtml(slashEscaped,{radiusMiles:50,locationLabel:'Grand Rapids, Minnesota'});assert.equal(escapedParsed.length,1);assert.equal(escapedParsed[0].id,'111222333444');assert.equal(escapedParsed[0].location_verified,true);
+
+const unicodeEscaped='<script>{"marketplace_listing_title":"Snowblower","location":"Grand Rapids, Minnesota","url":"https:\\u002F\\u002Fwww.facebook.com\\u002Fmarketplace\\u002Fitem\\u002F555666777888\\u002F"}</script>';
+const unicodeParsed=parseGuestHtml(unicodeEscaped,{radiusMiles:50,locationLabel:'Grand Rapids, MN'});assert.equal(unicodeParsed.length,1);assert.equal(unicodeParsed[0].url,'https://www.facebook.com/marketplace/item/555666777888/');assert.equal(unicodeParsed[0].location_verified,true);
+
+const encoded='<a href="https%3A%2F%2Fwww.facebook.com%2Fmarketplace%2Fitem%2F999888777666%2F">Marketplace</a><script>{"marketplace_listing_title":"Compressor","location":"Grand Rapids, MN"}</script>';
+const encodedParsed=parseGuestHtml(encoded,{radiusMiles:50,locationLabel:'Grand Rapids, Minnesota'});assert.equal(encodedParsed.length,1);assert.equal(encodedParsed[0].id,'999888777666');
+
+const wrongArea=parseGuestHtml('<script>{"marketplace_listing_title":"Saw","location":"Grand Rapids, MI"}</script><a href="/marketplace/item/777888999000/">x</a>',{radiusMiles:50,locationLabel:'Grand Rapids, Minnesota'});assert.equal(wrongArea.length,1);assert.equal(wrongArea[0].location_verified,false);
+
+const gated=parseGuestHtml('<html><body>Log in to Facebook to continue</body></html>',{radiusMiles:50,locationLabel:'Grand Rapids, Minnesota'});assert.equal(gated.length,0);
+
+console.log('PASS reseller-facebook-public-v265 guest hydration + strict public-location fixtures');
