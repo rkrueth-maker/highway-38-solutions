24ea42269811cf200e1d7d52d1f154d1625afe1e
rkrueth-maker <297361570+rkrueth-maker@users.noreply.github.com>
2026-09-02 02:34:09 -0500

    Use accepted dated cache build for UX polish

--- commercial-app/service-worker.js
+++ commercial-app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='h38-business-office-20260902-ux-polish-1';
+const CACHE_NAME='h38-business-office-20260902-0216';
 const PREVIOUS_EXAMPLE_COMMAND_CACHE_NAME='h38-business-office-20260824-0815';
 const OLDER_EXAMPLE_COMMAND_CACHE_NAME='h38-business-office-20260824-0410';
 const PREVIOUS_CONVERSATION_CACHE_NAME='h38-business-office-20260824-0155';
